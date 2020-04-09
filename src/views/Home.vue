90f6c5f887c194ba9f0169464a346d7e79fed01a
samsour <sam.sauer@hotmail.de>
2020-04-09 23:35:54 +0200

    Add tictactoe mechanic

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,16 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <tic-tac-toe />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import TicTacToe from '@/components/TicTacToe.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    TicTacToe
   }
 }
 </script>
